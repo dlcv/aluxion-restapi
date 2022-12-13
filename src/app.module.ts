45229b5ffe7c28a04d2fd58099d5ed7897da0b3c
Francisco De La Cruz Véliz <dlcv@live.com.ve>
2022-12-13 12:17:43 -0400

    Feat: install TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { FilesModule } from './files/files.module';
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [UsersModule, FilesModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    port: 3306,
+    host: 'localhost',
+    username: 'root',
+    password: '',
+    database: 'aluxion_api',
+    entities: [__dirname + '/**/*.entity{.ts, .js}'],
+    synchronize: true
+  }), UsersModule, FilesModule],
   controllers: [AppController],
   providers: [AppService],
 })
